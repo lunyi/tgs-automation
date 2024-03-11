420cf5e1c8d8392b33c054d8dfb43aab0c017e8a
lester.wei <ly.lester@rayprosoft.com>
2024-03-11 10:53:23 +0800

    minor fix

--- pkg/foo/foo_mock_test.go
+++ pkg/foo/foo_mock_test.go
@@ -3,24 +3,19 @@ package foo
 import (
 	"testing"
 
+	gomock "github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 )
 
 // REF: https://blog.kenwsc.com/posts/2020/discover-go-unit-test-stub-and-mock/
 
-type dbMock struct{}
-
-func newDbMock() *dbMock {
-	return &dbMock{}
-}
-
-func (d *dbMock) First(out interface{}) {
-	out.(*User).Age = 1
-}
-
-func TestFooDatabaseCustomMock(t *testing.T) {
-	want := 1
-	m := newDbMock()
+func TestFooDatabaseGomock(t *testing.T) {
+	expect := 1
+	var user User
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	m := NewMockDatabase(ctrl)
+	m.EXPECT().First(gomock.Eq(&user)).SetArg(0, User{Age: expect})
 	actual := fooDatabaseCaseIndirectCall(m)
-	assert.Equal(t, want, actual)
+	assert.Equal(t, expect, actual)
 }
