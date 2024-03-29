7cf7f1d2d415b5c326c60da509820f5a4db3c6c0
lester.wei <ly.lester@rayprosoft.com>
2024-03-29 17:24:08 +0800

    fix

--- cmd/momo-data/main.go
+++ cmd/momo-data/main.go
@@ -68,7 +68,7 @@ func createExcelPlayerRegistered(app postgresql.GetMomoDataInterface, brand stri
 		playerRegisteredInterface = append(playerRegisteredInterface, p)
 	}
 
-	playerRegisteredFile := fmt.Sprintf("%s_%s_register.xlsx", prefilename, brand)
+	playerRegisteredFile := fmt.Sprintf("%s_%s_註冊.xlsx", prefilename, brand)
 
 	err = createExcel(playerRegisteredInterface, playerRegisteredFile, populateSheetPlayerRegistered)
 	if err != nil {
@@ -83,7 +83,7 @@ func createExcelPlayerFirstDeposit(app postgresql.GetMomoDataInterface, brand st
 		log.LogFatal(err.Error())
 	}
 
-	playerFirstDepositFile := fmt.Sprintf("%s_%s_deposit.xlsx", prefilename, brand)
+	playerFirstDepositFile := fmt.Sprintf("%s_%s_首存.xlsx", prefilename, brand)
 
 	var playerFirstDepositInterface []interface{}
 	for _, p := range playerFirstDeposit {
