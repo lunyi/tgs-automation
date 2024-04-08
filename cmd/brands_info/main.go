51b5262fe4eee71787d825791d4819d48384289c
lester.wei <ly.lester@rayprosoft.com>
2024-04-08 14:40:32 +0800

    add getrooms

--- cmd/brands_info/main.go
+++ cmd/brands_info/main.go
@@ -36,6 +36,13 @@ func main() {
 	if err != nil {
 		log.LogInfo("Token:" + token)
 	}
+	var rooms []Room
+	rooms, err = getRooms(token)
+	if err != nil {
+		for _, room := range rooms {
+			log.LogInfo("Room:" + room.Title + " Token:" + room.Token)
+		}
+	}
 }
 
 func getToken() (string, error) {
@@ -96,8 +103,60 @@ func getToken() (string, error) {
 	return apiResponse.Token, nil
 }
 
+func getRooms(token string) ([]Room, error) {
+	url := "https://message.biatalk.cc/bot/v3/chatroom/metadata"
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return nil, err
+	}
+
+	var apiResponse RoomApiResponse
+	err = json.Unmarshal(body, &apiResponse)
+	if err != nil {
+		fmt.Println("Error unmarshalling response body:", err)
+		return "", err // Assuming your function returns a string and an error
+	}
+
+	// Now, apiResponse contains the unmarshalled data and you can use it
+	fmt.Printf("Response Status: %d\n", apiResponse.Status)
+	fmt.Printf("Message: %s\n", apiResponse.Message)
+
+	// Continue with your logic, possibly returning something based on apiResponse
+	return apiResponse.Rooms, nil
+}
+
 type ApiResponse struct {
 	Status  int    `json:"status"`
 	Message string `json:"message"`
 	Token   string `json:"token"`
+}
+type RoomApiResponse struct {
+	Status     int    `json:"status"`
+	Message    string `json:"message"`
+	TotalCount int    `json:"totalCount"`
+	Rooms      []Room `json:"rooms"`
+}
+
+// Room represents each room within the "rooms" array in the JSON response
+type Room struct {
+	Title string `json:"title"`
+	Token string `json:"token"`
 }
