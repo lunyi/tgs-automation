36d64952a3bd93d2be71f93acd249cd68a0fcec5
lester.wei <ly.lester@rayprosoft.com>
2024-04-08 13:31:51 +0800

    fix break line

--- cmd/brands_info/main.go
+++ cmd/brands_info/main.go
@@ -19,11 +19,11 @@ func main() {
 	message := ""
 
 	for _, brand := range brands {
-		message += brand.PlatformCode + `\n` +
-			`當日營收：` + fmt.Sprintf("%.2f", brand.DailyRevenueUSD) + `\n` +
-			`當日訂單數量：` + brand.DailyOrderCount + `\n` +
-			`當日活躍人數：` + fmt.Sprintf("%v", brand.ActiveUserCount) + `\n` +
-			`當月營收：` + fmt.Sprintf("%.2f", brand.CumulativeRevenueUSD) + `\n\n\n`
+		message += brand.PlatformCode + "\n" +
+			"當日營收：" + fmt.Sprintf("%.2f", brand.DailyRevenueUSD) + "\n" +
+			"當日訂單數量：" + brand.DailyOrderCount + "\n" +
+			"當日活躍人數：" + fmt.Sprintf("%d", brand.ActiveUserCount) + "\n" +
+			"當月營收：" + fmt.Sprintf("%.2f", brand.CumulativeRevenueUSD) + "\n\n\n"
 
 	}
 	log.LogInfo(message)
