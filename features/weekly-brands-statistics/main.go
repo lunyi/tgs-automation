62e93891f5d6331d544fbe22e5c58a521efac830
lester.wei <ly.lester@rayprosoft.com>
2024-04-16 15:58:59 +0800

    fix

--- features/weekly-brands-statistics/main.go
+++ features/weekly-brands-statistics/main.go
@@ -21,20 +21,18 @@ func main() {
 	app := postgresql.NewGetPlayersAdjustAmountInterface(config.Postgresql)
 	defer app.Close()
 
+	startDate := time.Now().AddDate(0, 0, -8).Format("20060102+8")
+	endDate := time.Now().AddDate(0, 0, -1).Format("20060102+8")
+	start := time.Now().AddDate(0, 0, -8).Format("0102")
+	end := time.Now().AddDate(0, 0, -2).Format("0102")
+
 	brands := []string{"MOPH", "MOVN2"}
 	file := xlsx.NewFile()
-	// Displaying all elements in the slice
+
 	for _, brand := range brands {
-
-		start := time.Now().AddDate(0, 0, -8).Format("0102")
-		end := time.Now().AddDate(0, 0, -2).Format("0102")
 		filename := fmt.Sprintf("%s-%s_%s.xlsx", start, end, brand)
-
-		startDate := time.Now().AddDate(0, 0, -8).Format("20060102+8")
-		endDate := time.Now().AddDate(0, 0, -1).Format("20060102+8")
-
-		exportPlayerAdjustFile(app, file, filename, brand, startDate, endDate)
 		exportPromotionDistributes(config, file, brand, filename, startDate, endDate)
+		exportPlayerAdjustFile(app, file, filename, brand, startDate, endDate)
 	}
 
 	sig := <-signals
@@ -53,9 +51,9 @@ func createSheet(file *xlsx.File, players []interface{}, excelFilename string, p
 	boldStyle := xlsx.NewStyle()
 	boldStyle.Font.Bold = true
 	headerRow := sheet.AddRow()
-	// Populating data
+
 	populate(headerRow, boldStyle, sheet, players, dataType)
-	// Save the file to the disk
+
 	err = file.Save(excelFilename)
 	if err != nil {
 		log.LogFatal(fmt.Sprintf("Save failed:: %s", err))
