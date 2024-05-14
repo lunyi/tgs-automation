a714d9d7938f44d76f840685012329e34eeb0049
lester.wei <ly.lester@rayprosoft.com>
2024-05-14 14:06:38 +0800

    refactor folder

--- features/create-site/main.go
+++ features/create-site/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"os/exec"
+	"text/template"
 
 	"tgs-automation/internal/log"
 	"tgs-automation/internal/util"
@@ -20,7 +25,7 @@ func main() {
 	router.GET("/healthz", healthCheckHandler)
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.GET("/token", tokenHandler)
-	router.POST("/site", AuthMiddleware(), getLobbyInfo)
+	router.POST("/site", AuthMiddleware(), createLobbyHandler)
 
 	err := router.Run(":8080")
 	if err != nil {
@@ -42,30 +47,87 @@ func tokenHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"token": tokenString})
 }
 
-func getLobbyInfo(c *gin.Context) {
+func createLobbyHandler(c *gin.Context) {
 	config := util.GetConfig()
+	request, err := parseRequestBody(c)
 
-	// Parse JSON body into struct
-	var request CreateSiteRequest
-	if err := c.BindJSON(&request); err != nil {
+	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request data", "details": err.Error()})
 		return
 	}
 
-	log.LogInfo(fmt.Sprintf("Request: %v", request))
-	log.LogInfo(fmt.Sprintf("Request lobbyTemplate: %v", request.LobbyTemplate))
-	log.LogInfo(fmt.Sprintf("Request brand code: %v", request.BrandCode))
-	log.LogInfo(fmt.Sprintf("Request lobbyTemplate: %v", request.Domain))
-	log.LogInfo(fmt.Sprintf("Request namespace: %v", request.NameSpace))
+	httpCode, response := handleCreateLobbyInfo(request, config)
 
+	if httpCode != http.StatusOK {
+		c.JSON(httpCode, response)
+		return
+	}
+
+	httpCode, response = handleKubectlApply(c)
+
+	c.JSON(httpCode, response)
+}
+
+func parseRequestBody(c *gin.Context) (CreateSiteRequest, error) {
+	var request CreateSiteRequest
+	if err := c.BindJSON(&request); err != nil {
+		log.LogError(fmt.Sprintf("Error parsing request body: %v", err))
+		return request, err
+	}
+	log.LogInfo(fmt.Sprintf("Request: %v", request))
+	return request, nil
+}
+
+func handleKubectlApply(lobby LobbyInfo, request CreateSiteRequest) (int, map[string]any) {
+	templateFile := fmt.Sprintf("lobby-%v.yaml", request.NameSpace)
+
+	content, err := ioutil.ReadFile(templateFile)
+	if err != nil {
+		log.LogError(fmt.Sprintf("Error reading template file:", err))
+		return http.StatusInternalServerError, gin.H{"error": "Error reading template file", "details": err.Error()}
+	}
+
+	// 創建模板並應用環境變量
+	tmpl, err := template.New("config").Parse(string(content))
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+		log.LogError(fmt.Sprintf("Error reading template file:", err))
+		return
+	}
+
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, os.Environ())
+	if err != nil {
+		fmt.Println("Error executing template:", err)
+		return
+	}
+
+	// 將結果寫入新文件
+	outputFile := "target.yaml"
+	err = ioutil.WriteFile(outputFile, buf.Bytes(), 0644)
+	if err != nil {
+		fmt.Println("Error writing file:", err)
+		return
+	}
+
+	// 執行 kubectl apply 命令
+	cmd := exec.Command("kubectl", "apply", "-f", outputFile)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println("Error running kubectl apply:", err)
+	}
+}
+
+func handleCreateLobbyInfo(request CreateSiteRequest, config util.TgsConfig) (int, map[string]any) {
 	// Fetch Docker image
 	dockerhubService := NewDockerImageService(config.Dockerhub)
 	image, err := dockerhubService.FetchDockerImage(request.LobbyTemplate)
 
 	if err != nil {
 		log.LogError(fmt.Sprintf("Error fetching docker image %v", err))
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not get docker image", "details": err.Error()})
-		return
+		return http.StatusInternalServerError, gin.H{"error": "Could not get docker image", "details": err.Error()}
 	}
 	fmt.Println("Image:", image)
 
@@ -73,7 +135,7 @@ func getLobbyInfo(c *gin.Context) {
 	brandId, err := postgresql.GetBrandId(config.CreateSiteDb, "MOPH")
 	if err != nil {
 		log.LogError(fmt.Sprintf("Error getting brand id %v", err))
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not get brand id", "details": err.Error()})
+		return http.StatusInternalServerError, gin.H{"error": "Could not get brand id", "details": err.Error()}
 	}
 	fmt.Println("Brand ID:", brandId)
 
@@ -81,13 +143,13 @@ func getLobbyInfo(c *gin.Context) {
 	token, err := getBrandToken(brandId, "staging", config.ApiUrl.BrandCert)
 	if err != nil {
 		log.LogError(fmt.Sprintf("Error getting brand token %v", err))
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not get brand token", "details": err.Error()})
+		return http.StatusInternalServerError, gin.H{"error": "Could not get brand token", "details": err.Error()}
 	}
 
 	lobby := &LobbyInfo{
 		BrandToken:  token,
 		DockerImage: image}
-	c.JSON(http.StatusOK, gin.H{"lobby": lobby})
+	return http.StatusOK, gin.H{"lobby": lobby}
 }
 
 type CreateSiteRequest struct {
