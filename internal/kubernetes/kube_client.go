c8f66b11919fd266c64ce7b1436a90c2851c1172
lesterwei <ly.lester@rayprosoft.com>
2024-07-16 15:41:57 +0800

    add k8s.io/client-go/tools/clientcmd

--- internal/kubernetes/kube_client.go
+++ internal/kubernetes/kube_client.go
@@ -8,6 +8,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func InitKubeClient(targetNamespace string) (*kubernetes.Clientset, error) {
